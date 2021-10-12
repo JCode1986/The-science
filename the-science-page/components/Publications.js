714a1d6194a4e076fe7383c894f3660e21b41982
JC <joseph.hangarter@yahoo.com>
2021-10-12 03:07:27 -0700

    added publications component

---
+++ the-science-page/components/Publications.js
@@ -0,0 +1,58 @@
+import React from 'react'
+
+export default function Publications() {
+    return (
+        <div className="bg-statisticsBgColor">
+            <div className="ml-28 mr-28">
+                <h2 className="text-4xl pt-16 pb-5">Publications</h2>
+                <p className="pb-11">We are dedicated to making accurate early detection tests for more cancers. Helio is currently engaged in <br/> multiple pivotal, multi-center trial in the US and China.</p>
+                <div className="flex justify-between">
+                    <div className="pr-4"style={{border:"1px solid grey"}}>
+                        <span>
+                            <h4 className="pt-5 pl-3 pb-2 text-darkgrey font-bold">
+                                Cellular Heterogeneity-Adjusted cLonal <br/> Methahylation (CHALM) improves prediction of <br/> gene expression
+                            </h4>
+                            <p className="pl-3 pb-2">
+                                Xu J, Shi J, Cui X, Li JJ, Goel A, Chen X, Issa <br/> JP, Su J, Li W.
+                            </p>
+                            <p className="pl-3 pb-5 text-learnMoreColor font-bold">
+                                Nature, March 2019
+                            </p>
+                        </span>
+                    </div>
+                    <div className="pr-4"style={{border:"1px solid grey"}}>
+                        <span>
+                            <h4 className="pt-5 pl-3 pb-2 text-darkgrey font-bold">
+                                Cellular Heterogeneity-Adjusted cLonal <br/> Methahylation (CHALM) improves prediction of <br/> gene expression
+                            </h4>
+                            <p className="pl-3 pb-2">
+                                Xu J, Shi J, Cui X, Li JJ, Goel A, Chen X, Issa <br/> JP, Su J, Li W.
+                            </p>
+                            <p className="pl-3 pb-5 text-learnMoreColor font-bold">
+                                Nature, March 2019
+                            </p>
+                        </span>
+                    </div>
+                    <div className="pr-4"style={{border:"1px solid grey"}}>
+                        <span>
+                            <h4 className="pt-5 pl-3 pb-2 text-darkgrey font-bold">
+                                Cellular Heterogeneity-Adjusted cLonal <br/> Methahylation (CHALM) improves prediction of <br/> gene expression
+                            </h4>
+                            <p className="pl-3 pb-2">
+                                Xu J, Shi J, Cui X, Li JJ, Goel A, Chen X, Issa <br/> JP, Su J, Li W.
+                            </p>
+                            <p className="pl-3 pb-5 text-learnMoreColor font-bold">
+                                Nature, March 2019
+                            </p>
+                        </span>
+                    </div>
+                </div>
+                <button className="font-bold py-2 px-4 text-darkgrey mt-7"
+                    style={{border:"1px solid darkgrey"}}
+                >
+                    See All Publications
+                </button>
+            </div>
+        </div>
+    )
+}
--- the-science-page/pages/index.js
+++ the-science-page/pages/index.js
@@ -1,9 +1,11 @@
-import Head from 'next/head'
-import Header from '../components/Header'
-import Evidence from '../components/Evidence'
-import Statistics from '../components/Statistics'
-import Advisors from '../components/Advisors'
-import Trials from '../components/Trials'
+import Head from 'next/head';
+import Header from '../components/Header';
+import Evidence from '../components/Evidence';
+import Statistics from '../components/Statistics';
+import Advisors from '../components/Advisors';
+import Trials from '../components/Trials';
+import Publications from '../components/Publications';
+import Footer from '../components/Footer';
 
 export default function Home() {
   return (
@@ -27,9 +29,12 @@ export default function Home() {
       
       {/* Trials */}
       <Trials />
+      
       {/* Publications */}
-
+      <Publications />
+      
       {/* Footer */}
+      <Footer/>
    </div>
   )
 }
