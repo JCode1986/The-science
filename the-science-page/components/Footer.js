692a731027492a81f6702af174942ee77d4d31ee
JC <joseph.hangarter@yahoo.com>
2021-10-12 12:29:02 -0700

    clean up

--- the-science-page/components/Footer.js
+++ the-science-page/components/Footer.js
@@ -3,7 +3,7 @@ import React from 'react'
 function Footer() {
     return (
         <div>
-            <p>Footer</p>
+            <p>Footer Here</p>
         </div>
     )
 }
--- the-science-page/components/Header.js
+++ the-science-page/components/Header.js
@@ -21,14 +21,14 @@ export default function Header() {
                     <li className="mr-10">
                         Resources
                     </li> 
-                    <div>
-                        <button className="bg-orange text-white font-bold py-2 px-4 rounded">
-                            Order Now
-                        </button> 
-                        <button className="border bg-white font-bold text-darkgrey py-2 px-4 rounded ml-5 mr-7">
-                            Log In
-                        </button> 
-                    </div>
+                <div>
+                    <button className="bg-orange text-white font-bold py-2 px-4 rounded">
+                        Order Now
+                    </button> 
+                    <button className="border bg-white font-bold text-darkgrey py-2 px-4 rounded ml-5 mr-7">
+                        Log In
+                    </button> 
+                </div>
             </ul>
         </header>
     )
